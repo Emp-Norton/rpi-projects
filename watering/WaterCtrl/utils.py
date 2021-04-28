b5502ec1bb75a7a0fc496c55d4f969446d0ea28e
James Jelenko <jamesjelenko@gmail.com>
2021-04-28 04:39:02 -0700

    add pump function to utils

--- watering/WaterCtrl/utils.py
+++ watering/WaterCtrl/utils.py
@@ -5,3 +5,8 @@ def get_analog_value(chan):
 	print("Raw ADC: ", chan.value)
 	print("ADC Voltage: ", chan.voltage)
 	return {'raw': chan.value, 'voltage': chan.voltage}
+
+def pump(pump_pin, seconds):
+	g.output(pump_pin, g.LOW)
+	time.sleep(seconds)
+	g.output(pump_pin, g.HIGH)
