f4dfec07edb87f29a57cc82ecc3ff93745e99319
James Jelenko <jamesjelenko@gmail.com>
2021-05-14 01:46:03 -0700

    add logging and utility functions to utils

--- watering/WaterCtrl/utils.py
+++ watering/WaterCtrl/utils.py
@@ -1,8 +1,25 @@
+import argparse
+import datetime
 import RPi.GPIO as g
 import time
 
+
+def write_to_log(data, path='./'):
+	with open(path, 'a') as file:
+		file.write('written {}\n{}\n'.format(datetime.datetime.now(), data))
+
+def read_all_pins(inputs, write_logs=False):
+	for channel in inputs:
+		sensor_info = inputs[channel]
+		readings = get_analog_value(sensor_info['sensor'])
+
+		message = "Reading from {}:\n\n{}\n\nRAW: {}\nVOLTAGE: {}\n\n".format(datetime.datetime.now, sensor_info['name'], reading['raw'], reading['voltage'])
+		print("writing readings to log")
+		print(message)
+		write_to_log(message)
+
 def get_analog_value(chan):
-	print("Raw ADC: ", chan.value)
+	print("Raw ADC: ", str(chan.value))
 	print("ADC Voltage: ", chan.voltage)
 	return {'raw': chan.value, 'voltage': chan.voltage}
 
@@ -11,3 +28,17 @@ def pump(pump_pin, seconds):
 	g.output(pump_pin, g.LOW)
 	time.sleep(seconds)
 	g.output(pump_pin, g.HIGH)
+
+
+def get_args():
+	argp = argparse.ArgumentParser()
+	argp.add_argument('--p0', help="This argument sets which pin will measure the plant or plant group provided")
+	argp.add_argument('--p1', help="This argument sets which pin will measure the plant or plant group provided")
+	argp.add_argument('--p2', help="This argument sets which pin will measure the plant or plant group provided")
+	argp.add_argument('--p3', help="This argument sets which pin will measure the plant or plant group provided")
+	argp.add_argument('--p4', help="This argument sets which pin will measure the plant or plant group provided")
+	argp.add_argument('--p5', help="This argument sets which pin will measure the plant or plant group provided")
+	argp.add_argument('--p6', help="This argument sets which pin will measure the plant or plant group provided")
+	argp.add_argument('--p7', help="This argument sets which pin will measure the plant or plant group provided")
+
+	return argp.parse_args()
