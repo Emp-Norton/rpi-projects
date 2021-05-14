a24a702e9d5be6e023033a5060bd8a23cfbf4027
James Jelenko <jamesjelenko@gmail.com>
2021-05-14 01:46:20 -0700

    move get_args_ to utils

--- watering/WaterCtrl/runwatering.py
+++ watering/WaterCtrl/runwatering.py
@@ -1,25 +1,11 @@
 import setup as s
 
 from vars import PUMP_CTRL_PIN, NUM_INPUTS
-from utils import get_analog_value, pump
+from utils import get_analog_value, pump, read_all_pins, write_to_log
 
 inputs = {}
 
-if __name__ == 'main':
-	inputs = get_args()
-
-
-def get_args():
-	argp = argparse.ArgumentParser()
-	argp.add_argument('--p0', help="This argument sets which pin will measure the plant or plant group provided")
-	argp.add_argument('--p1', help="This argument sets which pin will measure the plant or plant group provided")
-	argp.add_argument('--p2', help="This argument sets which pin will measure the plant or plant group provided")
-	argp.add_argument('--p3', help="This argument sets which pin will measure the plant or plant group provided")
-	argp.add_argument('--p4', help="This argument sets which pin will measure the plant or plant group provided")
-	argp.add_argument('--p5', help="This argument sets which pin will measure the plant or plant group provided")
-	argp.add_argument('--p6', help="This argument sets which pin will measure the plant or plant group provided")
-	argp.add_argument('--p7', help="This argument sets which pin will measure the plant or plant group provided")
-
-	return argp.parse_args()
+if __name__ == '__main__':
+	inputs = s.run_setup()
 
 print(inputs)
