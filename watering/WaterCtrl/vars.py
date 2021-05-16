77f16b15ce3e5ea5ac5ad2761229ed648693fb3a
James Jelenko <jamesjelenko@gmail.com>
2021-05-16 03:16:06 -0700

    update vars with todos

--- watering/WaterCtrl/vars.py
+++ watering/WaterCtrl/vars.py
@@ -1,6 +1,9 @@
 ''' Default values for pupms in prototype. There's a much better way but it's low priority '''
 
+# TODO: If drip irrigation not effective, not implementable in the remaining time, or not measurable, make this support 1:1 pump:watering_group
 PUMP_CTRL_PIN = 14
+# TODO: create defaults for watering groups? This magic number won't last beyond testing
 NUM_INPUTS = 5
+# TODO: Upate this after testing
 DEFAULT_PUMP_SECONDS = 3
 
