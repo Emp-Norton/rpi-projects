f3d193061a29cb727c0341dafa92f109a9d4fb59
James Jelenko <jamesjelenko@gmail.com>
2021-06-03 00:01:44 -0700

    remove print statements, add envs, fix some bugs with logging

--- watering/WaterCtrl/utils.py
+++ watering/WaterCtrl/utils.py
@@ -2,39 +2,44 @@ import argparse
 from datetime import datetime
 import os
 import time
-
+from decouple import config
 import RPi.GPIO as g
 
+workspace = config('workspace')
+project_path = config('projectpath')
 
-def write(path, data, now):
-	# TODO Error handling and just choose one place to display write-time
-	print("Write_log--path: {}".format(path))
-
-	with open(path, 'a+') as file:
-		file.write('Recorded: {}\n{}\n'.format(now, data))
-
-
-def write_to_log(data):
-	# TODO: Fix this to be more clear in output, better organized, better naming conventions, and dynamic file path
-	now = datetime.now()
+def get_time_parts(now):
 	str_datetime = now.strftime('%Y-%m-%d - %I:%M:%s %p')
 	time_parts = str_datetime.split(' ')
 	date = time_parts[0]
 	time = "-".join(time_parts[2:])
 
-	#project_path = os.environ['/watering/WaterCtrl']
-	workspace = os.environ['workspace']
-	path="{}/watering/WaterCtrl/logs/sensors/{}/".format(workspace, date)
-	print("Write_to_log--path: {}".format(path))
-	# TODO fix this shit, only works on repeat, which isn't right.
+	return {'date': date, 'time': time}
+
+def write(path, data, now):
+	try:
+		with open(path, 'a+') as file:
+			file.write('Recorded: {}\n{}\n'.format(now, data))
+	except Exception as e:
+		print("Failed with {}".format(e))
+
+def write_to_log(data):
+	# TODO: Fix this to be more clear in output, better organized, better naming conventions, and dynamic file path (only if pulling to generic log_helper?)
+	now = datetime.now()
+	date = get_time_parts(now)['date']
+	time = get_time_parts(now)['time']
+
+	path="{}{}/logs/sensors/{}".format(workspace, project_path, date)
+	file_path = "{}/{}.txt".format(path, time)
+
+	# TODO write helper runction to create directory and then just do `if not os.path.... then create -- end block, drop into "write" like you normally would. 
 	if os.path.exists(path):
-		write(path+"{}.txt".format(time), data, time)
+		write(file_path, data, time)
 	else:
-		new_dir = "{}/{}/logs/sensors/{}"
-		os.makedirs(new_dir.format(workspace, '/watering/WaterCtrl', date))
+		new_dir = "{}{}/logs/sensors/{}".format(workspace, project_path, date)
+		os.makedirs(new_dir)
 		file_name = "{}/{}.txt".format(new_dir, time)
 		write(file_name, data, time)
-
 
 
 def read_all_pins(inputs, write_logs=False):
@@ -43,11 +48,9 @@ def read_all_pins(inputs, write_logs=False):
 		name = channel['name']
 		sensor = channel['sensor']
 		readings = get_analog_value(sensor)
-		message = "\nReading from {}:\n{}\n{}\n\n".format(name, sensor.value, sensor.voltage)
-		print(message)
+		message = "Reading from {}:\n{}\n{}\n\n".format(name, sensor.value, sensor.voltage)
 
 		if write_logs:
-			print("writing readings to log")
 			if name is not None:
 				write_to_log(message)
 
