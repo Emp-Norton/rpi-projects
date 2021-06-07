516950751dc6d31e07ef6f13de13901d08cf7f16
James Jelenko <jamesjelenko@gmail.com>
2021-06-07 12:36:04 -0700

    fix tab/space issue and add prefixing to logging for tests

--- watering/WaterCtrl/utils.py
+++ watering/WaterCtrl/utils.py
@@ -26,7 +26,7 @@ def write(path, data, now):
 	except Exception as e:
 		print("Failed with {}".format(e))
 
-def write_to_log(data, module):
+def write_to_log(data, module, *prefix):
 	# TODO: Fix this to be more clear in output, better organized, better naming conventions, and dynamic file path (only if pulling to generic log_helper?)
 	now = datetime.now()
 	date = get_time_parts(now)['date']
@@ -49,31 +49,31 @@ def read_all_pins(inputs, write_logs=False):
         log_module = modules.SENSOR
         # TODO: add default paths and filename patterns for various components (pumps, sensors, etc)
         for channel in list(inputs.values()):
-		name = channel['name']
-		sensor = channel['sensor']
-	        readings = get_analog_value(sensor)
-		message = "Reading from {}:\n{}\n{}\n\n".format(name, sensor.value, sensor.voltage)
-		print(message)
-		if write_logs:
-			if name is not None:
-				write_to_log(message, log_module)
+                name = channel['name']
+                sensor = channel['sensor']
+                readings = get_analog_value(sensor)
+                message = "Reading from {}:\n{}\n{}\n\n".format(name, sensor.value, sensor.voltage)
+                print(message)
+                if write_logs:
+                        if name is not None:
+                                write_to_log(message, log_module)
 
 def get_analog_value(chan):
-# TODO Error handling and logging
-	return {'raw': chan.value, 'voltage': chan.voltage}
+        # TODO Error handling and logging
+        return {'raw': chan.value, 'voltage': chan.voltage}
 
 # todo fix this issienwitj seconds not being loggrd
 def pump(pump_pin, seconds):
         log_module = module.PUMP
-	log_message = "{} pump \#{} - {} seconds at {}\n".format(actions.START, pump_pin, seconds, datetime.now()), "pumps")
+        log_message = "{} pump \#{} - {} seconds at {}\n".format(actions.START, pump_pin, seconds, datetime.now())
         write_to_log(log_message, log_module)
-	print(log_message))
+        print(log_message)
 
         g.output(pump_pin, g.LOW)
-	time.sleep(seconds)
-	g.output(pump_pin, g.HIGH)
+        time.sleep(seconds)
+        g.output(pump_pin, g.HIGH)
 
-	log_message = "{} pump \#{} - {} seconds at {}\n".format(actions.STOP, pump_pin, seconds, datetime.now())
+        log_message = "{} pump \#{} - {} seconds at {}\n".format(actions.STOP, pump_pin, seconds, datetime.now())
         print(log_message)
         write_to_log(log_message, log_module)
 
@@ -83,14 +83,14 @@ def run_all_pumps(pump_pins, seconds, **custom_times):
         This is partially due to ease of development, but also because in a small reservoir they can interfere with each other's operation.
         """
         log_module = modules.PUMP
-	pump_pin_times = {str(pin): seconds for pin in pump_pins}
+        pump_pin_times = {str(pin): seconds for pin in pump_pins}
 
-	if custom_times:
+        if custom_times:
                 log_message ="Using custom times: {}\n".format(custom_times)
                 write_to_log(log_message, log_module)
-                print(log_message))
-		for pin in custom_times:
-			pump_pin_times[pin] = custom_times[pin]
+                print(log_message)
+                for pin in custom_times:
+                        pump_pin_times[pin] = custom_times[pin]
 
         for pin in pump_pin_times:
                 time = pump_pin_times[pin]
