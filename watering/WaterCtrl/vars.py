38ae914a3dd2dd3edacc19a55161701caacaf738
James Jelenko <jamesjelenko@gmail.com>
2021-06-07 12:42:51 -0700

    update default values in vars file

--- watering/WaterCtrl/vars.py
+++ watering/WaterCtrl/vars.py
@@ -2,8 +2,15 @@
 
 # TODO: If drip irrigation not effective, not implementable in the remaining time, or not measurable, make this support 1:1 pump:watering_group
 PUMP_CTRL_PIN = 14
+
+PUMP_PINS = [14, 15, 18] 
+# Pump Layout IIRC:
+## 14 is first pump, going to Tradescantia Zebrinas and Small Succulents, 
+## 15 is middle pump, presumably larger succulents and a pothos?
+## 18 is 12V pump powered by wall plug, going to herbs, prayer plant,pothos? 
+
 # TODO: create defaults for watering groups? This magic number won't last beyond testing
 NUM_INPUTS = 5
 # TODO: Upate this after testing
-DEFAULT_PUMP_SECONDS = 3
+DEFAULT_PUMP_SECONDS = 30
 
