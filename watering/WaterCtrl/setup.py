11e8e5a0a49e64f34524d22754e152e9228ff7c8
James Jelenko <jamesjelenko@gmail.com>
2021-06-08 23:33:16 -0700

    fixfunction signature and other stuff

--- watering/WaterCtrl/setup.py
+++ watering/WaterCtrl/setup.py
@@ -5,7 +5,7 @@ import digitalio
 import RPi.GPIO as g
 import sys
 import time
-import vars
+import var
 
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn as AI
