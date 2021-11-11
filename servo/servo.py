c79b8536561311256fa36ac6f5482c5d13d1ff68
James Jelenko <jamesjelenko@gmail.com>
2021-11-10 18:40:27 -0800

    initial servo tests

--- servo/servo.py
+++ servo/servo.py
@@ -1,9 +1,21 @@
 from decouple import config
 from gpiozero import Servo
 
-pin = config('servo_pin')
-
-servo = Servo(pin)
+pins = config('pins', [13, 18])
 
 class ServoWrapper(Servo):
-    def __init__(self, pin):
+    def __init__(self, pins=pins):
+        for pin in pins:
+            self.servoes.append(super().__init__(int(pin)))
+
+    def stop(self):
+        self.value = None
+
+    def setup_servo(selfpin):
+        servo = ServoWrapper(pin)
+        servo.stop()
+        return servo
+
+    def setup(self, pins=pins):
+        [self.servos.append(self.setup_servo(pin)) for pin in pins]
+
